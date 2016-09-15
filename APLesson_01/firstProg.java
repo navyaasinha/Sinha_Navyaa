9aec865204ae771bd5911a724d7d7a0365c66fec
navyaasinha <navyaasinha@gmail.com>
2016-09-15 13:44:48 -0700

    completed lesson 1

---
+++ APLesson_01/firstProg.java
@@ -0,0 +1,27 @@
+public class firstProg
+{
+	public static void main (String[]args)
+	{
+		System.out.println("Hello World");
+		System.out.println("Hello Again");
+		System.out.println(" \"\"  \"\"  \"\"  ");
+		
+		
+		System.out.println("      :");
+		System.out.println("   ___:____     |\"\\/\"| ");
+		System.out.println(" ,'        `.    \\  /");
+		System.out.println("  |  O        \\___/  |");
+		System.out.println(" ^~^~^~^~^~^~^~^~^~^~^~^~");
+		
+		
+		
+		
+		
+		System.out.println(" _,-._");
+		System.out.println("/ \\_/ \\");
+		System.out.println(">-(_)-<");    
+		System.out.println("\\_/ \\_/");
+		
+	}
+}
+
