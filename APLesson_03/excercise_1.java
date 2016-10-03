b3ed7dd673dec5596a95d124c219094abdaff29a
navyaasinha <navyaasinha@gmail.com>
2016-10-03 13:01:11 -0700

    fixed lesson 3

--- APLesson_03/excercise_1.java
+++ APLesson_03/excercise_1.java
@@ -12,8 +12,8 @@ public class excercise_1
 		System.out.println("Ew." + name + "is the worst name I've ever heard." );
 		
 		System.out.println("How old are you?");
-		int num = keyboard.nextInt();//leaves a return character left over
-		System.out.println("Wow!" + num + "is SO old. You might die pretty soon." );
+		int age = keyboard.nextInt();//leaves a return character left over
+		System.out.println("Wow!" + age + "is SO old. You might die pretty soon." );
 		keyboard.nextLine();//eats the extra return character
 		System.out.println("What do you do for fun?");
 		String hobby = keyboard.nextLine();
@@ -24,9 +24,9 @@ public class excercise_1
 		System.out.println("You have such bad music taste." + music + "is the worst.");
 		
 		System.out.println("How many siblings do you have?");
-		c
-		System.out.println("I feel bad for those" + num + "siblings");
-		
+		int sibs = keyboard.nextInt();
+		keyboard.nextLine();
+		System.out.println("I feel bad for those " + sibs + " siblings");
 		System.out.println("What do you want to be when you grow up?");
 		String job = keyboard.nextLine();
 		System.out.println("What makes you think you are good enough to be a " + job );
--- APLesson_04/excercise_1.java
+++ APLesson_04/excercise_1.java
@@ -2,7 +2,11 @@ public class excercise_1
 {
 	public static void main(String[]args)
 	{
-		System.out.printf("%10s  %10.2", "test");
+		System.out.printf("\n%10s  02394820", "thisexam");
+	}
+	
+	public void format (String word, double number)
+	{
 		System.out.printf("\n%10s  02394820", "thisexam");
 	}
 }
