0dd7791ae6c4d8a62e47642782601d73a6d98a6e
navyaasinha <navyaasinha@gmail.com>
2017-02-27 14:25:20 -0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	APLesson_13/ToyStore.class

--- APLesson_13/ToyRunner.java
+++ APLesson_13/ToyRunner.java
@@ -2,9 +2,9 @@ public class ToyRunner
 {
 	public static void main(String[]args)
 	{
-		ToyStore newToy = new ToyStore("Hotwheel, Car, G.I.Joe, AF, PennyRacer, Car, Matchbox, Car, Star Wars, AF, Pullback, Car, Star Wars, AF");
-		System.out.println(newToy);
-		System.out.println(newToy.getMostFrequentToy());
-		System.out.println(newToy.getMostFrequentType());
+		AFigure bob = new AFigure("Jones");
+		Car racer = new Car("Ferrari");
+		System.out.println(bob);
+		System.out.println(racer);
 	}
 }
---
+++ APLesson_13/ToyStoreRunner.java
@@ -0,0 +1,10 @@
+public class ToyStoreRunner
+{
+	public static void main(String[]args)
+	{
+		ToyStore newToy = new ToyStore("Hotwheel, Car, G.I.Joe, AF, PennyRacer, Car, Matchbox, Car, Star Wars, AF, Pullback, Car, Star Wars, AF");
+		System.out.println(newToy);
+		System.out.println(newToy.getMostFrequentToy());
+		System.out.println(newToy.getMostFrequentType());
+	}
+}
